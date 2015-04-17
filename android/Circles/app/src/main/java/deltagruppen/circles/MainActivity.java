f96f22980ef22f35aadb3760ae1696f18ac8275d
Dante <dante.tnc.johansson@gmail.com>
2015-04-17 19:23:06 +0200

    Added popup window for changing language, not functional yet.
    Added a popup window that doesn't have any functionality yet. The design
    should be changed as well.

--- android/Circles/app/src/main/java/deltagruppen/circles/MainActivity.java
+++ android/Circles/app/src/main/java/deltagruppen/circles/MainActivity.java
@@ -1,16 +1,26 @@
 package deltagruppen.circles;
 
+
 import android.content.pm.ActivityInfo;
 import android.support.v7.app.ActionBarActivity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.LinearLayout;
+import android.widget.PopupWindow;
+import android.view.ViewGroup.LayoutParams;
 
 
 public class MainActivity extends ActionBarActivity {
+    Button showLangWindow;
+    ImageButton showLangWindowIm;
+    LinearLayout layout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +29,9 @@ public class MainActivity extends ActionBarActivity {
         setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_main);
+        layout = new LinearLayout(this);
+        windowInit();
+        prepareButtons();
     }
 
 
@@ -43,6 +56,37 @@ public class MainActivity extends ActionBarActivity {
 
         return super.onOptionsItemSelected(item);
     }
+
+    public void windowInit(){
+        showLangWindow = (Button) findViewById(R.id.button4);
+        showLangWindowIm = (ImageButton) findViewById(R.id.imageButton5);
+    }
+
+    public void prepareButtons(){
+        showLangWindow.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                LayoutInflater inflater = (LayoutInflater) getBaseContext().getSystemService(LAYOUT_INFLATER_SERVICE);
+                View langPopup = inflater.inflate(R.layout.layout_lang_popup, null);
+                final PopupWindow pop = new PopupWindow(langPopup, LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT);
+                pop.showAtLocation(layout, 0, 700, 700);
+            }
+        });
+        showLangWindowIm.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                LayoutInflater inflater = (LayoutInflater) getBaseContext().getSystemService(LAYOUT_INFLATER_SERVICE);
+                View langPopup = inflater.inflate(R.layout.layout_lang_popup, null);
+                final PopupWindow pop = new PopupWindow(langPopup, LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT);
+                pop.showAtLocation(layout, 0, 700, 700);
+            }
+        });
+    }
+
+
+ /*   public void changeLanguage () {
+
+    }*/
 
     public void showPiInfo(MenuItem item) {
         Intent intent = new Intent(this, PiInfo.class);

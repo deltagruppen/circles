571135763dbeecd49a4a615b8b798e9bfb098fca
Christoffer Alenskog <christoffer@hush.ai>
2015-05-11 00:43:05 +0200

    Algorithm is able to handle more intersections than one but only uses the first closed curve for calculations. There's need for som more bugfixing before merge with develop branch. Sometimes the closest circle shows a suspicious ressult and sometimes the curve fails to register altogether.

--- android/Circles/app/src/main/java/perfectpi/circles/ImperfectCircle.java
+++ android/Circles/app/src/main/java/perfectpi/circles/ImperfectCircle.java
@@ -3,12 +3,13 @@ package perfectpi.circles;
 import android.graphics.Point;
 import android.graphics.PointF;
 
+import org.apache.commons.math3.geometry.euclidean.threed.Line;
 import org.apache.commons.math3.geometry.euclidean.twod.Vector2D;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 import java.util.ListIterator;
-import java.util.Objects;
 
 import perfectpi.circles.math.LineSegment;
 
@@ -21,6 +22,9 @@ public class ImperfectCircle
     private List<PointF> input;
     private List<PointF> points;
     private List<LineSegment> segments;
+    private PointF firstIntersection;
+    private PointF lastPoint;
+    private LineSegment firstLineSegment;
 
     // Value for checking proximity between points will depend on the
     // stroke width of drawn path.
@@ -60,26 +64,66 @@ public class ImperfectCircle
         }
     }
 
+    /**
+     * Find the first intersection and create a list of all points from the first intersection
+     * back to the same point.
+     * @return True if intersection exists, false if not.
+     */
     private boolean findIntersection() {
 
-        int i, j;
-        for (i = 0; i < segments.size(); i++) {
-            LineSegment s1 = segments.get(i);
-            for (j = i + 2; j < segments.size(); j++) {
-                LineSegment s2 = segments.get(j);
-                PointF intersection = s1.getIntersection(s2);
-                if (intersection != null) {
-                    // Create a list of points containing the first point after the intersection(i+1)
-                    // and the last one before the intersection (j).
-                    points = new ArrayList<>(input.subList(i + 1, j));
-                    // The intersection should be the first and the last point in the list
-                    points.add(0, intersection);
-                    points.add(intersection);
+        int startIndex = 0, finalIndex = 0;
+        PointF intersection;
 
-                    // Move on when an intersection is found.
-                    return true;
+        for (int i = 0; i < segments.size(); i++) {
+            LineSegment s1 = segments.get(i);
+            for (int j = i + 2; j < segments.size(); j++) {
+                LineSegment s2 = segments.get(j);
+                intersection = s1.getIntersection(s2);
+                if (intersection != null) {
+
+                    List<LineSegment> tempSegments;
+                    // If the first intersection is unassigned assign the first intersection and the first line segment
+                    // otherwise more than one intersection exist and the figure must be further examined.
+                    if (firstIntersection == null) {
+                        firstIntersection = intersection;
+                        firstLineSegment = s1;
+
+                        // Remember where the first intersection exists in case it's the only one.
+                        startIndex = i; finalIndex = j;
+
+                        // The first intersection is found. Only keep segments in between. Discard the
+                        // first ten segments to prevent too small distances between the first and the
+                        // second intersection.
+
+                        tempSegments = new ArrayList<>(segments.subList(i+10, j + 1));
+                        segments.clear();
+                        segments.addAll(tempSegments);
+
+
+                    } else {
+                        // More than one intersections exist.
+                        // Add the first ten points to the beginning of the list.
+                        points = new ArrayList<>(input.subList(startIndex, 11));
+                        lastPoint = intersection;
+                        return true; // TODO optimization: Add all points up to the second intersection and reorder segments.
+                    }
+                    // Look for the next intersection, if there are no more the closed curve has been established
+                    // otherwise further work will be needed.
                 }
             }
+        }
+
+        // Only one intersection exists hence no further examination is needed
+        if ((lastPoint = firstIntersection) != null) {
+
+            // Add all points from the intersection back to the intersection. Clear all former points.
+            points = new ArrayList<>(input.subList(startIndex,finalIndex+1));
+            points.add(0, firstIntersection);
+            points.add(firstIntersection);
+
+            // Exactly one intersection was found.
+            return true;
+
         }
         return false;
     }
@@ -93,7 +137,126 @@ public class ImperfectCircle
      * @return A new list of points containing the first closed curve.
      */
     private void closedCurve() {
+        // Build the list of the correct closed curve until the first intersection
+        // is reached. If the lastPoint is found and equal to the first intersection
+        // the closed curve is completely built. If there's only one intersection
+        // nextIntersection() will never be reached since lastPoint will already be
+        // equal to the first intersection.
+        while (lastPoint != firstIntersection) {
+            nextIntersection();
+        }
     }
+
+    /**
+     * Find the next intersection and add all points between that one and
+     * the last one.
+     *
+     * @return The list of points so far.
+     */
+    private void nextIntersection() {
+
+        PointF intersection = null;
+
+        // Keeps track of the iterated points. Will be used to add points in the list points when needed.
+        List<PointF> pointStack = new ArrayList<>();
+
+        for (int i = 0; i < segments.size(); i++) {
+            LineSegment s1 = segments.get(i);
+            pointStack.add(new PointF((float)s1.p1.getX(), (float)s1.p1.getY()));
+
+            // Always look for the first intersection before the others to see if the end of the
+            // closed curve has been reached.
+            if ((intersection = s1.getIntersection(firstLineSegment)) != null) {
+                points.addAll(pointStack);
+                points.add(intersection);
+                lastPoint = firstIntersection;
+                return;
+            }
+
+            for (int j = i + 2; j < segments.size(); j++) {
+                LineSegment s2 = segments.get(j);
+                intersection = s1.getIntersection(s2);
+
+                if (intersection != null) {
+
+                    // Assign the intersection to the last point for future comparison with the first intersection.
+                    lastPoint = intersection;
+
+                    // If there exists an intersection between the current line segments the next direction
+                    // should be determined, and the list of line segments arranged accordingly.
+                    // The intersection should also be added at the end of the list of points
+                    // and finally the whole list should be returned.
+                    double angle = getAngle(s1, s2);
+
+                    // If the angle between the two line segments is less than 180 degrees the next point in
+                    // the closed curve after the intersection is s2.p2. Otherwise the next point is s2.p1
+                    // and the segments should be arranged in reversed order to be processed correctly.
+                    if (angle < Math.PI) {
+
+                        // Add the past points
+                        points.addAll(pointStack);
+
+                        // Add the intersection to the list points.
+                        points.add(intersection);
+
+                        // Skip over all segments from i to j + 1.
+                        List<LineSegment> tempSegments = new ArrayList<>(segments.subList(j + 1, segments.size()));
+                        segments.clear();
+                        segments.addAll(tempSegments);
+                        return;
+
+                    } else if (angle > Math.PI) {
+                        points.add(intersection);
+
+                        // Extract the part of the closed curve that is backwards
+                        List<LineSegment> backwards = new ArrayList<>(segments.subList(i, j));
+
+                        // Extract the rest of the curve
+                        List<LineSegment> rest = new ArrayList<>(segments.subList(j, segments.size()));
+
+                        // Reverse the part that should be reversed
+                        Collections.reverse(backwards);
+
+                        // TODO Remove the end of the last segment in the backward part and the beginning of
+                        // the first segment in the rest part so that they end and begin at the intersection.
+
+                        // Add the rest to the reversed part
+                        backwards.addAll(rest);
+
+                        // Finally set the segments list as the correctly ordered segments
+                        segments.clear();
+                        segments.addAll(backwards);
+
+                        // Return the intersection so the closed curve method can determine when the
+                        // end of the closed curve is reached.
+                        return;
+                    }
+                }
+            }
+        }
+
+        // TODO Remove all segments that are on the dead end path. And restructure segments (only way to end up here)
+
+    }
+
+    public double getAngle(LineSegment s1, LineSegment s2) {
+
+        double angle;
+
+        // Normalize the two line segments to represent vectors going from the origin
+        PointF v1 = new PointF((float)(s1.p2.getX() - s1.p1.getX()), (float)(s1.p2.getY() - s1.p1.getY()));
+        PointF v2 = new PointF((float)(s2.p2.getX() - s2.p1.getX()), (float)(s2.p2.getY() - s2.p1.getY()));
+
+        double v1_length = Math.sqrt(Math.pow(v1.x, 2) + Math.pow(v1.y, 2));
+        double v2_length = Math.sqrt(Math.pow(v2.x, 2) + Math.pow(v2.y, 2));
+
+        // The angle between the vectors is calculated from the formula cos(v) = v1 . v2 / (||v1|| * ||v2||).
+        // It says that cos(v) is equal to the dot product of the two vectors over the product of their lengths.
+        angle = Math.acos((v1.x * v2.x + v1.y * v2.y)/(v1_length*v2_length));
+
+        return angle;
+    }
+
 
     /**
      * If no intersection is found the algorithm will try and find
